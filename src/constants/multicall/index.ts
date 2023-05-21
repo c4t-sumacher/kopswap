4c1e6c1b73c6b7ef48be413aae6fae01a36ea338
Mark Su <marksu@Marks-MBP13-2020.local>
2023-05-21 08:40:25 +0800

    interface v2 for mumbai

--- src/constants/multicall/index.ts
+++ src/constants/multicall/index.ts
@@ -1,4 +1,4 @@
-import { ChainId } from '@uniswap/sdk'
+import { ChainId } from 'markuniswap-sdk'
 import MULTICALL_ABI from './abi.json'
 
 const MULTICALL_NETWORKS: { [chainId in ChainId]: string } = {
@@ -6,7 +6,8 @@ const MULTICALL_NETWORKS: { [chainId in ChainId]: string } = {
   [ChainId.ROPSTEN]: '0x53C43764255c17BD724F74c4eF150724AC50a3ed',
   [ChainId.KOVAN]: '0x2cc8688C5f75E365aaEEb4ea8D6a480405A48D2A',
   [ChainId.RINKEBY]: '0x42Ad527de7d4e9d9d011aC45B31D8551f8Fe9821',
-  [ChainId.GÖRLI]: '0x77dCa2C955b15e9dE4dbBCf1246B4B85b651e50e'
+  [ChainId.GÖRLI]: '0x77dCa2C955b15e9dE4dbBCf1246B4B85b651e50e',
+  [ChainId.MUMBAI]: '0x223152215Afd2A2Bd27C68080AE20fd8085336F6'
 }
 
 export { MULTICALL_ABI, MULTICALL_NETWORKS }
